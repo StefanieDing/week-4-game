9c552f32c54720ed621afee73cafa4d032bb04d4
Stefanie Ding <StefanieDing@StefaniDingsMBP.home>
2016-08-20 19:38:28 -0400

    unable to select buttons separately. trying new verion of game js

--- assets/javascript/game_v2.js
+++ assets/javascript/game_v2.js
@@ -1,13 +1,7 @@
 //Music
 var music = document.createElement('audio');
 music.setAttribute('src', '../week-4-game/assets/sounds/FOB-Immortals.mp3');
-
-var charName = ['Baymax', 'Hiro','GoGo', 'Honey-Lemon', 'Wasabi-No-Ginger', 'Fred']
-var charHP = [2000, 1650, 1850, 1700, 1800, 1500];
-var charImg = ['../week-4-game/assets/images/Baymax.png', '../week-4-game/assets/images/Hiro.png', '../week-4-game/assets/images/GoGo.png', '../week-4-game/assets/images/HoneyLemon.png', '../week-4-game/assets/images/Wasabi.png', '../week-4-game/assets/images/Fred.png'];
-var charHit = [200, 165, 185, 170, 180, 150];
-var charSpecialAttack = [250, 215, 235, 220, 230, 200];
-
+var characters = ['baymax', 'hiro', 'gogo', 'honeyLemon', 'wasabi', 'fred'];
 var defenders = []; //or set to 5?
 
 
@@ -15,40 +9,71 @@ var defenders = []; //or set to 5?
 $(".playButton").on("click", function(){
 	music.play();
 });
-
 $(".pauseButton").on("click", function(){
 	music.pause();
 });
 
 //Creating buttons *WIP
-for(var i = 0; i < charName.length; i++){
-	//display name and HP *WIP
-	var character = $('<img>');
-	character.attr('src', charImg[i]);
-	//character.text(charName[i] + charHP[i]);
-	character.addClass('charImg');
-	character.attr({'data-hp': charHP[i]});
-	character.attr({'data-hit': charHit[i]});
-	character.attr({'data-spattack': charSpecialAttack[i]});
-	character.attr({'data-name': charName[i]});
-	$('#startBtn').append(character);
+var baymax = {
+	name: 'Baymax',
+	image: '<img src =../week-4-game/assets/images/Baymax.png'>,
+	hp: 2000,
+	hit: 200,
+	specialAttack: 250,
 }
 
-pickCharacter();
+var hiro = {
+	name: 'Hiro Takachiho',
+	image: '<img src =../week-4-game/assets/images/Hiro.png'>,
+	hp: 1650,
+	hit: 165,
+	specialAttack: 215,
+}
+
+var gogo = {
+	name: 'Gogo Tomago',
+	image: '<img src =../week-4-game/assets/images/GoGo.png'>,
+	hp: 1850,
+	hit: 185,
+	specialAttack: 235,
+}
+
+var honeyLemon = {
+	name: 'Honey Lemon',
+	image: '<img src =../week-4-game/assets/images/HoneyLemon.png'>,
+	hp: 1700,
+	hit: 170,
+	specialAttack: 220,
+}
+
+var wasabi = {
+	name: 'Wasabi-No-Ginger',
+	image: '<img src =../week-4-game/assets/images/Wasabi.png'>,
+	hp: 1800,
+	hit: 180,
+	specialAttack: 200,
+}
+
+var fred = {
+	name: 'Fred',
+	image: '<img src =../week-4-game/assets/images/Fred.png'>,
+	hp: 1500,
+	hit: 150,
+	specialAttack: 215,
+}
+$('#baymax').html(baymax);
+//Sets character to User Character from click
+$('#baymax').on("click", function(){
+		chooseCharacter(baymax);
+	}
 
 function chooseCharacter(){
-//Sets first click to User character 
-$('.charImg').on('click', function(){
-	console.log($(this).data('name'));
-	//var userChar = this;
-	//userChar.addClass('userStyle');
-	//$('userChar').html(userChar);
-});
-}
+
+
 
 //Sets the other characters into enemy character div.
 //chooseDefender();
-
+}
 //clicking enemy sends them to battleMode function. 
 function chooseDefender(){
 	var defenderAttack
